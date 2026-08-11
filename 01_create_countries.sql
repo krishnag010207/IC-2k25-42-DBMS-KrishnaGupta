68371ecb2f30b577d0d3adb641068ac7c0d55eec
krishnag010207 <krishnagupta171105@gmail.com>
2026-08-11 11:11:03 +0530

    Add files via upload

---
+++ 01_create_countries.sql
@@ -0,0 +1,52 @@
+Enter password: ****
+Welcome to the MySQL monitor.  Commands end with ; or \g.
+Your MySQL connection id is 10
+Server version: 8.0.46 MySQL Community Server - GPL
+
+Copyright (c) 2000, 2026, Oracle and/or its affiliates.
+
+Oracle is a registered trademark of Oracle Corporation and/or its
+affiliates. Other names may be trademarks of their respective
+owners.
+
+Type 'help;' or '\h' for help. Type '\c' to clear the current input statement.
+
+mysql> create database dbms_lab;
+Query OK, 1 row affected (0.15 sec)
+
+mysql> use dbms_lab;
+Database changed
+mysql> select database();
++------------+
+| database() |
++------------+
+| dbms_lab   |
++------------+
+1 row in set (0.00 sec)
+
+mysql> create table countries (
+    -> country_id int,
+    -> country_name varchar(50),
+    -> region_id int
+    -> );
+Query OK, 0 rows affected (0.11 sec)
+
+mysql> show tables;
++--------------------+
+| Tables_in_dbms_lab |
++--------------------+
+| countries          |
++--------------------+
+1 row in set (0.03 sec)
+
+mysql> desc countries;
++--------------+-------------+------+-----+---------+-------+
+| Field        | Type        | Null | Key | Default | Extra |
++--------------+-------------+------+-----+---------+-------+
+| country_id   | int         | YES  |     | NULL    |       |
+| country_name | varchar(50) | YES  |     | NULL    |       |
+| region_id    | int         | YES  |     | NULL    |       |
++--------------+-------------+------+-----+---------+-------+
+3 rows in set (0.01 sec)
+
+mysql>
