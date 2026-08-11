eadad1fdd548e32dfcf8e21fd3dc6ed61fc84d75
krishnag010207 <krishnagupta171105@gmail.com>
2026-08-11 12:36:07 +0530

    Remove MySQL monitor commands from SQL script

--- 02_create_countries_if_not_exist.sql
+++ 02_create_countries_if_not_exist.sql
@@ -1,16 +1,3 @@
-Enter password: ****
-Welcome to the MySQL monitor.  Commands end with ; or \g.
-Your MySQL connection id is 11
-Server version: 8.0.46 MySQL Community Server - GPL
-
-Copyright (c) 2000, 2026, Oracle and/or its affiliates.
-
-Oracle is a registered trademark of Oracle Corporation and/or its
-affiliates. Other names may be trademarks of their respective
-owners.
-
-Type 'help;' or '\h' for help. Type '\c' to clear the current input statement.
-
 mysql> use dbms_lab
 Database changed
 mysql> select database();
